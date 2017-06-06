b73ca5b7e590cb6f9db1b686b6f0e350dc7aab82
lesykgg <lesykgg@gmail.com>
2017-06-06 23:55:52 +0300

    Completed task according to the variant

--- homework.rb
+++ homework.rb
@@ -1,6 +1,7 @@
-VARIANT = 0
-
+VARIANT = 18
+# "AA21-ACBD-CD-VB-123AB"
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+	alphabet = ('A'..'Z').to_a
+	[alphabet.sample(2).join + rand(0..99).to_s.rjust(2, '0'), alphabet.sample(4).join, alphabet.sample(2).join,
+	 alphabet.sample(2).join, rand(0..999).to_s.rjust(3, '0') + alphabet.sample(2).join].join('-')
 end
