333a37d93ba825ecdeef498263e62d43d94b42d2
dennis <you@example.com>
2017-06-07 00:03:12 +0300

    Variant 7

--- homework.rb
+++ homework.rb
@@ -1,6 +1,9 @@
-VARIANT = 0
+VARIANT = 7
 
 def generate
   # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  letters = ['A'..'Z'].map(&:to_a).flatten
+  string1 = (0...3).map { letters[rand(letters.length)] }.join
+  string2 = (0...3).map { letters[rand(letters.length)] }.join
+  string1 + '-' + rand(0..9999999).to_s + '-' + string2
 end
