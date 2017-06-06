b549d091d76d6c45f05741b7b9ff30421ed03e2a
dennis <you@example.com>
2017-06-07 00:23:02 +0300

    VARIANT 7

--- homework.rb
+++ homework.rb
@@ -3,7 +3,10 @@ VARIANT = 7
 def generate
   # put your solution here (delete the line below)
   letters = ['A'..'Z'].map(&:to_a).flatten
-  string1 = (0...3).map { letters[rand(letters.length)] }.join
-  string2 = (0...3).map { letters[rand(letters.length)] }.join
-  string1 + '-' + rand(0..9999999).to_s + '-' + string2
+  string = []
+  2.times do |i|
+   i = (0...3).map { letters[rand(letters.length)] }.join
+   string << i
+  end
+  string[0] + '-' + rand(100000..9999999).to_s + '-' + string[1]
 end
