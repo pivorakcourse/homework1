da4163c0341c72b87d3298fe5750e82ed3162536
Ivan Sheremeta <iisheremeta@gmail.com>
2017-06-08 00:36:38 +0300

    Homework#1 done

--- homework.rb
+++ homework.rb
@@ -1,6 +1,10 @@
-VARIANT = 0
+VARIANT = 8
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  letters = *("A".."Z")
+
+  letter1, letter2 = 2.times.map { letters.sample(3).join }
+  num1, num2 = 2.times.map { rand(100..999).to_s }
+
+  [letter1, num1, letter2, num2].join("-")
 end
