346faf394ea3bf85e7ae7415f61fba1b4a2ea433
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-06-08 00:13:34 +0300

    Add 12th variant

--- homework.rb
+++ homework.rb
@@ -1,6 +1,15 @@
-VARIANT = 0
+VARIANT = 12
+NUM_OF_LETTERS = 6
+NUM_OF_DIGITS = 8
+DIGITS_SPLIT_PLACE = 6
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  # pattern: "ABCDEF-123456-98"
+  # chunk of additional code to add (hopefully) more functionality:
+  temp_str = ''
+  NUM_OF_DIGITS.times { temp_str += rand(0...10).to_s }
+  # the main code:
+  ('a'..'z').to_a.sample(NUM_OF_LETTERS).join.upcase + '-' +
+                temp_str.slice(0...DIGITS_SPLIT_PLACE) + '-' +
+                  temp_str.slice(DIGITS_SPLIT_PLACE...NUM_OF_DIGITS)
 end
