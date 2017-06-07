314f5f590602ac108c9eaa2bc4d08631573c1dc6
aleksacastle <aaklochko@gmail.com>
2017-06-07 13:59:00 +0300

    Homework 17
    Done

--- homework.rb
+++ homework.rb
@@ -1,6 +1,13 @@
-VARIANT = 0
+VARIANT = 17
 
-def generate
+  def generate
   # put your solution here (delete the line below)
-  rand(1000).to_s + '-' + rand(1000).to_s
+  #template "1XC1-A11D-1CDVB-123"
+
+  rand(10).to_s+(65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+rand(10).to_s+"-"+ #1XC1-
+  (65 + rand(26)).chr.to_s+rand(10).to_s+rand(10).to_s+(65 + rand(26)).chr.to_s+"-"+ #A11D-
+  rand(10).to_s+(65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+
+  (65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+"-"+ #1CDVB-
+  rand(10).to_s+rand(10).to_s+rand(10).to_s #123
+
 end
