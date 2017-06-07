0e1211833eb624087ece5063967489aae9306bca
oleg.matviiv <oleg.matviiv@gmail.com>
2017-06-07 13:08:49 +0300

    pivorak homework #1

--- homework.rb
+++ homework.rb
@@ -1,6 +1,8 @@
-VARIANT = 0
+VARIANT = 15
 
+# Run test 100 times to ensure that there is no duplicates:
+#   for i in {0..100};do ruby test/homework_test.rb | grep failures| grep -v '0 failures';done
+# It should fit this pattern "123456-ZXCE-ABCD"
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  rand(100000...999999).to_s + '-' + (("A".."Z").to_a).sample(4).join + '-' + (("A".."Z").to_a).sample(4).join
 end
