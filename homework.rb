bcd4228dd0209f51a7939eb6ff2150ef1ca82ae8
sontsevolosa <sontsevolosa@gmail.com>
2017-06-07 10:15:06 +0300

    Add 'generate' method which return  return a random number

--- homework.rb
+++ homework.rb
@@ -1,6 +1,12 @@
 VARIANT = 9
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  first_random_numbers = Array.new(3) { rand(1...9) }.join('')
+  second_random_numbers = Array.new(3) { rand(1...9) }.join('')
+
+  letters_array = ('A'..'Z').to_a.flatten
+  first_random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
+  second_random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
+
+  "#{first_random_numbers}" + "-" + "#{first_random_letters}" + "-" + "#{second_random_letters}" + "-" + "#{second_random_numbers}"
 end
