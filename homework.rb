b766587c97e3df78213a52bbdb54d5112aca2ed6
Sontsevolosa <sontsevolosa@gmail.com>
2017-06-08 19:49:02 +0300

    Remove unnecessary code

--- homework.rb
+++ homework.rb
@@ -6,5 +6,5 @@ def generate
   letters_array = ('A'..'Z').to_a.flatten
   random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
 
-  "#{random_numbers}" + "-" + "#{random_letters}" + "-" + "#{random_letters}" + "-" + "#{random_numbers}"
+  "#{random_numbers}-#{random_letters}-#{random_letters}-#{random_numbers}"
 end
