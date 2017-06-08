5173f6815205c26537c635a0c5a0591426a99552
Denys Makarenko <makarenko.denys@gmail.com>
2017-06-09 00:06:00 +0300

    Code was refactored according to the comments

--- homework.rb
+++ homework.rb
@@ -1,5 +1,9 @@
 VARIANT = 6
 
 def generate
-  rand(10..99).to_s + '-' + rand(1000000..9999999).to_s + '-' + [*"A".."Z"].sample(3).join
+  first_part = rand(10..99).to_s
+  second_part = rand(1000000..9999999).to_s
+  third_part = [*"A".."Z"].sample(3).join
+
+  "#{first_part}-#{second_part}-#{third_part}"
 end
