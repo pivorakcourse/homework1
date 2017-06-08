2e717fa49f280932f7f1a75558012793ad8f8526
aleksacastle <aaklochko@gmail.com>
2017-06-08 22:00:19 +0300

    sveredyuk changes

--- homework.rb
+++ homework.rb
@@ -7,7 +7,7 @@ VARIANT = 17
   rand_char = lambda{|x| ("A".."Z").to_a.sample(x).join}  #generating random letter
   one = rand_int.call(1) + rand_char.call(2) + rand_int.call(1) #1XC1
   two = rand_char.call(1) + rand_int.call(2) + rand_char.call(1) #A11D
-  three = rand_int.call(1) + rand_char.call(3) #1CDVB
+  three = rand_int.call(1) + rand_char.call(4) #1CDVB
   four = rand_int.call(3) #123
   "#{one}-#{two}-#{three}-#{four}"
   end
