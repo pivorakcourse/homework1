6c44e212cb847b0eea9e1c6fe48f5d170e5bd391
aleksacastle <aaklochko@gmail.com>
2017-06-08 13:37:54 +0300

    magic number disappear
    cause magic

--- homework.rb
+++ homework.rb
@@ -1,13 +1,20 @@
 VARIANT = 17
 
+  def rand_int
+    #generate random number
+    rand(10).to_s
+  end
+
+  def rand_char
+    #generating random letter
+    (65 + rand(26)).chr.to_s
+  end
+
   def generate
   # put your solution here (delete the line below)
   #template "1XC1-A11D-1CDVB-123"
-
-  rand(10).to_s+(65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+rand(10).to_s+"-"+ #1XC1-
-  (65 + rand(26)).chr.to_s+rand(10).to_s+rand(10).to_s+(65 + rand(26)).chr.to_s+"-"+ #A11D-
-  rand(10).to_s+(65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+
-  (65 + rand(26)).chr.to_s+(65 + rand(26)).chr.to_s+"-"+ #1CDVB-
-  rand(10).to_s+rand(10).to_s+rand(10).to_s #123
-
-end
+  rand_int + rand_char + rand_char  +rand_int + "-" + #1XC1-
+  rand_char + rand_int + rand_int + rand_char + "-"+ #A11D-
+  rand_int + rand_char + rand_char + rand_char + rand_char + "-"+ #1CDVB-
+  rand_int + rand_int + rand_int #123
+  end
