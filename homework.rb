94b05a6747b897320105a49cf9042e6ac1393522
lesykgg <lesykgg@gmail.com>
2017-06-08 20:58:44 +0300

    Fixed complexity

--- homework.rb
+++ homework.rb
@@ -1,7 +1,24 @@
 VARIANT = 18
-# "AA21-ACBD-CD-VB-123AB"
+#pattern "AA21-ACBD-CD-VB-123AB"
+
+def gen_literas(length)
+  alphabet = ('A'..'Z').to_a
+  alphabet.sample(length).join
+end
+
+def gen_digits(length)
+  if length == 2
+  	rand(0..99).to_s.rjust(2, '0')
+  elsif length == 3
+  	rand(0..999).to_s.rjust(3, '0')
+  end
+end
+
 def generate
-	alphabet = ('A'..'Z').to_a
-	[alphabet.sample(2).join + rand(0..99).to_s.rjust(2, '0'), alphabet.sample(4).join, alphabet.sample(2).join,
-	 alphabet.sample(2).join, rand(0..999).to_s.rjust(3, '0') + alphabet.sample(2).join].join('-')
+  first_block = gen_literas(2) + gen_digits(2)
+  second_block = gen_literas(4)
+  third_block = gen_literas(2)
+  fourth_block = gen_literas(2)
+  fifth_block = gen_digits(3) + gen_literas(2)
+  [first_block, second_block, third_block, fourth_block, fifth_block].join('-')
 end
