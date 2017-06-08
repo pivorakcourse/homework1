ea9f49085cd4cc3b6ae016cc0a9235bab8e95037
sontsevolosa <sontsevolosa@gmail.com>
2017-06-08 19:34:07 +0300

    Delete repetitions from 'generate' method

--- homework.rb
+++ homework.rb
@@ -1,12 +1,10 @@
 VARIANT = 9
 
 def generate
-  first_random_numbers = Array.new(3) { rand(1...9) }.join('')
-  second_random_numbers = Array.new(3) { rand(1...9) }.join('')
+  random_numbers = Array.new(3) { rand(1...9) }.join('')
 
   letters_array = ('A'..'Z').to_a.flatten
-  first_random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
-  second_random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
+  random_letters = Array.new(3) { letters_array[rand(letters_array.length)] }.join('')
 
-  "#{first_random_numbers}" + "-" + "#{first_random_letters}" + "-" + "#{second_random_letters}" + "-" + "#{second_random_numbers}"
+  "#{random_numbers}" + "-" + "#{random_letters}" + "-" + "#{random_letters}" + "-" + "#{random_numbers}"
 end
