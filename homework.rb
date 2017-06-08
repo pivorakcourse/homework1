32e147166820ea8b749818176ab4fed5195daa9f
oleg.matviiv <oleg.matviiv@gmail.com>
2017-06-08 19:06:38 +0300

    get rid of magic numbers

--- homework.rb
+++ homework.rb
@@ -1,8 +1,8 @@
 VARIANT = 15
 
-# Run test 100 times to ensure that there is no duplicates:
-#   for i in {0..100};do ruby test/homework_test.rb | grep failures| grep -v '0 failures';done
 # It should fit this pattern "123456-ZXCE-ABCD"
 def generate
-  rand(100000...999999).to_s + '-' + (("A".."Z").to_a).sample(4).join + '-' + (("A".."Z").to_a).sample(4).join
+  numbers = -> { rand(100_000..999_999).to_s }
+  letters = -> { ('A'..'Z').to_a.sample(4).join }
+  [numbers.call, letters.call, letters.call].join('-')
 end
