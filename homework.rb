6e1df1c6c7f9365d756fd5a27ef3c88470d7939c
aleksacastle <aaklochko@gmail.com>
2017-06-08 13:44:26 +0300

    magic numbers

--- homework.rb
+++ homework.rb
@@ -13,7 +13,7 @@ VARIANT = 17
   def generate
   # put your solution here (delete the line below)
   #template "1XC1-A11D-1CDVB-123"
-  rand_int + rand_char + rand_char  +rand_int + "-" + #1XC1-
+  rand_int + rand_char + rand_char + rand_int + "-" + #1XC1-
   rand_char + rand_int + rand_int + rand_char + "-"+ #A11D-
   rand_int + rand_char + rand_char + rand_char + rand_char + "-"+ #1CDVB-
   rand_int + rand_int + rand_int #123
