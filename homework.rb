010b29c52eba43a05a43fd93100a4a74230fd427
admin <terekh.vasyl@gmail.com>
2017-06-08 15:57:06 +0300

    refactored code

--- homework.rb
+++ homework.rb
@@ -5,6 +5,6 @@ def generate
   alphabet = ('A'..'Z').to_a
   alphabet_count = alphabet.count
 
-  %Q(#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)}-
-  #{rand(1000...10000)}-#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)})
+  %Q(#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)}-\
+#{rand(1000...10000)}-#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)})
 end
