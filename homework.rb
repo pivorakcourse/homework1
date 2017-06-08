435cb9b5951a458372711ef6681da2ad122875db
antl-mark <a.markevuch@ukr.net>
2017-06-08 22:47:48 +0300

    homework1 V2

--- homework.rb
+++ homework.rb
@@ -1,6 +1,15 @@
-VARIANT = 0
+VARIANT = 2
+RANGE_NUMBERS = Array.new(4, (0..9).to_a )
+RANGE_LETTERS = Array.new(4, ('A'..'Z').to_a)
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  "#{numbers}-#{letters}-#{numbers}"
+end
+
+def numbers
+  RANGE_NUMBERS.map{ |elem| elem.sample }.join
+end
+
+def letters
+  RANGE_LETTERS.map{ |elem| elem.sample }.join
 end
