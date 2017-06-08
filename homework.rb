3decbfc40fe721a2e3dc9a5d0c101b5d0917d194
lesykgg <lesykgg@gmail.com>
2017-06-08 21:12:25 +0300

    Got rid of magic numbers

--- homework.rb
+++ homework.rb
@@ -1,24 +1,21 @@
 VARIANT = 18
-#pattern "AA21-ACBD-CD-VB-123AB"
+# pattern "AA21-ACBD-CD-VB-123AB"
 
-def gen_literas(length)
+def generate_literas(length)
   alphabet = ('A'..'Z').to_a
   alphabet.sample(length).join
 end
 
-def gen_digits(length)
-  if length == 2
-  	rand(0..99).to_s.rjust(2, '0')
-  elsif length == 3
-  	rand(0..999).to_s.rjust(3, '0')
-  end
+def generate_digits(length)
+  rand(0..999).to_s[0...length].rjust(length, '0')
 end
 
 def generate
-  first_block = gen_literas(2) + gen_digits(2)
-  second_block = gen_literas(4)
-  third_block = gen_literas(2)
-  fourth_block = gen_literas(2)
-  fifth_block = gen_digits(3) + gen_literas(2)
+  first_block = generate_literas(2) + generate_digits(2)
+  second_block = generate_literas(4)
+  third_block = generate_literas(2)
+  fourth_block = generate_literas(2)
+  fifth_block = generate_digits(3) + generate_literas(2)
+
   [first_block, second_block, third_block, fourth_block, fifth_block].join('-')
 end
