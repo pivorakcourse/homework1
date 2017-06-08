ccbd25417771824ef8adb53f63e80a7165722a7a
Volodymyr <kokoroinkan@gmail.com>
2017-06-08 11:46:25 +0300

    remove puts

--- homework.rb
+++ homework.rb
@@ -1,12 +1,12 @@
 VARIANT = 1
 
 def generate
-  ch  = lambda { |range| (0...range).map { ('A'..'Z').to_a[rand(26)] }.join}
-  num = lambda { |range| (0...range).map { rand(9) }.join}
+  ch  = lambda { |range| (0...range).map { ('A'..'Z').to_a[rand(26)] }.join }
+  num = lambda { |range| (0...range).map { rand(9) }.join }
   result_1  = "#{num.call(4)}-#{num.call(4)}-#{num.call(4)}"
 end
 
 generate
 
-# puts result_10 = "#{ num.call(3)}-#{ch.call(3)}-#{num.call(3)}-#{ch.call(3) }"
-# puts result_20 = "#{ num.call(2)}-#{num.call(1)}#{ch.call(4)}-#{num.call(5)}-#{ch.call(4)}#{num.call(2) }"
+# result_10 = "#{ num.call(3)}-#{ch.call(3)}-#{num.call(3)}-#{ch.call(3) }"
+# result_20 = "#{ num.call(2)}-#{num.call(1)}#{ch.call(4)}-#{num.call(5)}-#{ch.call(4)}#{num.call(2) }"
