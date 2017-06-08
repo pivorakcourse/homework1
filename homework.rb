a85a387b1adf48bf16c9ce8da780fd77b508c464
hmaherovska <maher.olena96@gmail.com>
2017-06-08 15:37:54 +0300

    removed magic numbers

--- homework.rb
+++ homework.rb
@@ -1,12 +1,16 @@
 VARIANT = 19
-
+ONE_DIGIT_NUMBER = 1..9
+TWO_DIGIT_NUMBER = 10..99
+THREE_DIGIT_NUMBER = 100..999
 def generate
   [
-    rand(10..99),
+    rand(TWO_DIGIT_NUMBER),
     ('a'..'z').to_a.sample(2).join,
-    rand(1..9),
+    rand(ONE_DIGIT_NUMBER),
     ('a'..'z').to_a.sample(4).join,
-    rand(100..999),
+    rand(THREE_DIGIT_NUMBER),
     ('a'..'z').to_a.sample(2).join
   ].join('-').upcase
 end
+
+p generate
