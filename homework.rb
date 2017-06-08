882ccb4d98a285ca60610674feed579cf3002c77
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-06-08 21:54:01 +0300

    Use times.map instead of plain times

--- homework.rb
+++ homework.rb
@@ -4,15 +4,12 @@ NUM_PART1_LENGTH = 6
 NUM_PART2_LENGTH = 2
 
 def random_num_str(str_length)
-  arr = Array.new
-  str_length.times { arr << rand(0...10) }
-  arr.join
+  str_length.times.map { rand(0...10) }.join
 end
 
 def generate
   # pattern: "ABCDEF-123456-98"
-  letters = ''
-  LETTERS_LENGTH.times { letters += ('A'..'Z').to_a.sample }
+  letters = LETTERS_LENGTH.times.map { ('A'..'Z').to_a.sample }.join
   num_part1 = random_num_str(NUM_PART1_LENGTH)
   num_part2 = random_num_str(NUM_PART2_LENGTH)
   "#{letters}-#{num_part1}-#{num_part2}"
