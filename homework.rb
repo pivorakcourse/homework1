0c1e343b90198962dd2257a0e274839465fde189
mrsOwl <whitecapricorni@gmail.com>
2017-06-09 00:10:51 +0300

    fixes homework1

--- homework.rb
+++ homework.rb
@@ -1,7 +1,15 @@
 VARIANT = 1
 
 def generate 
-  i, j, k = rand(1000...10000), rand(1000...10000), rand(1000...10000)
-  
-   "#{i}" + "-" + "#{j}" + "-" + "#{k}"
+
+  def random_12_numbers 
+      return rand(1000..10000)
+  end
+
+
+  first_random_four_numbers = random_12_numbers()
+  second_random_four_numbers = random_12_numbers()
+  third_random_four_numbers = random_12_numbers()
+
+  return "#{first_random_four_numbers}-#{second_random_four_numbers}-#{third_random_four_numbers}"
 end
