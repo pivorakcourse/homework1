17c9c0d7c4a49900d5a9d23d784789f0d30cddce
dennis <you@example.com>
2017-06-08 16:40:55 +0300

    fix of random numbers

--- homework.rb
+++ homework.rb
@@ -4,9 +4,12 @@ def generate
   # put your solution here (delete the line below)
   letters = ['A'..'Z'].map(&:to_a).flatten
   string = []
+
   2.times do |i|
-   i = (0...3).map { letters[rand(letters.length)] }.join
-   string << i
+    i = (0..2).map { letters[rand(letters.length)] }.join
+    string << i
   end
-  string[0] + '-' + rand(100000..9999999).to_s + '-' + string[1]
+
+  string[0] + '-' + rand(1000000..9999999).to_s + '-' + string[1]
+
 end
