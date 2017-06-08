4bb0678b0a0e3d3f71875182c852cf084ea91fe7
admin <terekh.vasyl@gmail.com>
2017-06-08 15:48:45 +0300

    refactored code

--- homework.rb
+++ homework.rb
@@ -1,10 +1,10 @@
 VARIANT = 4
 
 def generate
-  #alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  #alphabet[rand(26)]+alphabet[rand(26)]+rand(10...100).to_s+"-"+rand(1000...10000).to_s+"-"+alphabet[rand(26)]+  alphabet[rand(26)]+rand(10...100).to_s
+
   alphabet = ('A'..'Z').to_a
   alphabet_count = alphabet.count
-  #alphabet[rand(alphabet_count)]+alphabet[rand(alphabet_count)]+rand(10...100).to_s+"-"+rand(1000...10000).to_s+"-"+alphabet[rand(alphabet_count)]+  alphabet[rand(alphabet_count)]+rand(10...100).to_s
-  %Q(#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)}-#{rand(1000...10000)}-#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)})
+
+  %Q(#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)}-
+  #{rand(1000...10000)}-#{alphabet[rand(alphabet_count)]}#{alphabet[rand(alphabet_count)]}#{rand(10...100)})
 end
