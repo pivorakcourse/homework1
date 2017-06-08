4cc35e290a8bd58be2301f0cc8c1373439515e2a
Mykola Matsko <mykola.matsko@gmail.com>
2017-06-08 15:51:40 +0300

    Homework 10

--- homework.rb
+++ homework.rb
@@ -1,6 +1,11 @@
-VARIANT = 0
+VARIANT = 10
+# 10 "123-ABC-987-ABC"
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  letter = ("A".."Z").to_a
+
+  letters = -> { letter.sample(3).join }
+  numbers = -> { rand(100..999).to_s }
+
+  [numbers.call, letters.call, numbers.call, letters.call].join("-")
 end
