9e04220e06480a8118c1f0e890229ec8074ffbac
Zbyslav <zbyshek99@gmail.com>
2017-06-08 22:00:48 +0300

    Create homework.rb
    - magic numbers removed
    - `.times` rewrote in one line
    - used interpolation in `generate`

--- homework.rb
+++ homework.rb
@@ -1,20 +1,20 @@
 VARIANT = 16
 ALPHABET = ("A".."Z").to_a
+NUM_BLOCK_LENGTH = 5
+LETTER_BLOCK_LENGTH = 4
 
 def rand_letters_block
   letters_block = Array.new
-  4.times do letters_block << ALPHABET[rand(ALPHABET.length)]
-  end
+  LETTER_BLOCK_LENGTH.times{ letters_block << ALPHABET[rand(ALPHABET.length)] }
   letters_block.join
 end
 
 def rand_numbers_block
   numbers_block = Array.new
-  5.times do numbers_block << rand(0..9)
-  end
+  NUM_BLOCK_LENGTH.times{ numbers_block << rand(0..9) }
   numbers_block.join
 end
 
 def generate
-  rand_letters_block + '-' + rand_letters_block + '-' + rand_numbers_block + '-' + rand_numbers_block
+  "#{rand_letters_block}-#{rand_letters_block}-#{rand_numbers_block}-#{rand_numbers_block}"
 end
