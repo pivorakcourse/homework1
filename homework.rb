35ab5bbac3bdc649ab72092d0371dce7678ad570
harusan <kokoroinkan@gmail.com>
2017-06-08 11:37:23 +0300

    all done (i hope) :cake:

--- homework.rb
+++ homework.rb
@@ -1,6 +1,12 @@
-VARIANT = 0
+VARIANT = 1
 
 def generate
-  # put your solution here (delete the line below)
-  rand(1000000).to_s + '-' + rand(1000000).to_s
+  ch  = lambda { |range| (0...range).map { ('A'..'Z').to_a[rand(26)] }.join}
+  num = lambda { |range| (0...range).map { rand(9) }.join}
+  result_1  = "#{num.call(4)}-#{num.call(4)}-#{num.call(4)}"
 end
+
+generate
+
+# puts result_10 = "#{ num.call(3)}-#{ch.call(3)}-#{num.call(3)}-#{ch.call(3) }"
+# puts result_20 = "#{ num.call(2)}-#{num.call(1)}#{ch.call(4)}-#{num.call(5)}-#{ch.call(4)}#{num.call(2) }"
