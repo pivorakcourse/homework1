e7830257c7714b48e0852896c21916013ebd0850
dennis <you@example.com>
2017-06-08 20:35:16 +0300

    add rand numbers var

--- homework.rb
+++ homework.rb
@@ -2,13 +2,12 @@ VARIANT = 7
 
 def generate
   # put your solution here (delete the line below)
-  letters = []
- 
-  2.times do |i|
-  	i = ('A'..'Z').to_a.sample(3).join
-  	letters << i
+  result = []
+  2.times do |letters|
+    letters = ('A'..'Z').to_a.sample(3).join
+    result << letters
   end
- 
-  letters[0] + '-' + rand(1000000..9999999).to_s + '-' + letters[1]
-
+  numbers = rand(1000000..9999999).to_s
+  result.insert(1, numbers)
+  result.join('-')
 end
