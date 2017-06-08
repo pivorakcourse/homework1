2ba980e6caa5817cf6e664af6d050b4f53eecd18
Ivan Sheremeta <iisheremeta@gmail.com>
2017-06-08 15:35:35 +0300

    stabby lambda usage

--- homework.rb
+++ homework.rb
@@ -3,8 +3,10 @@ VARIANT = 8
 def generate
   letters = *("A".."Z")
 
-  letter1, letter2 = 2.times.map { letters.sample(3).join }
-  num1, num2 = 2.times.map { rand(100..999).to_s }
+  letter = -> { letters.sample(3).join }
+  number = -> { rand(100..999).to_s }
 
-  [letter1, num1, letter2, num2].join("-")
+  [letter.call, number.call, letter.call, number.call].join("-")
 end
+
+
