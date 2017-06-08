b60f60e36614e9b8ee4064b740db04e81b64de1f
Mykola Matsko <mykola.matsko@gmail.com>
2017-06-08 18:11:30 +0300

    second v10 example

--- homework.rb
+++ homework.rb
@@ -2,10 +2,19 @@ VARIANT = 10
 # 10 "123-ABC-987-ABC"
 
 def generate
+  # letter = ("A".."Z").to_a
+
+  # letters = -> { letter.sample(3).join }
+  # numbers = -> { rand(100..999).to_s }
+
+  # [numbers.call, letters.call, numbers.call, letters.call].join("-")
+
+
+
   letter = ("A".."Z").to_a
 
-  letters = -> { letter.sample(3).join }
-  numbers = -> { rand(100..999).to_s }
+  letters = lambda { |x| letter.sample(x).join }
+  number = lambda { rand(1..9).to_s }
 
-  [numbers.call, letters.call, numbers.call, letters.call].join("-")
+  number.call+number.call+number.call+"-"+letters.call(3)+"-"+number.call+number.call+number.call+"-"+letters.call(3)
 end
