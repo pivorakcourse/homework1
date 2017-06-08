70c97be44ab46db4ae03cd59d08d81888a3651ab
aleksacastle <aaklochko@gmail.com>
2017-06-08 21:54:23 +0300

    sveredyuk comments

--- homework.rb
+++ homework.rb
@@ -1,21 +1,13 @@
 VARIANT = 17
-  A_ENCODING = 65
-  TOTAL_LETTERS_ALPHABET = 26
-  def rand_int
-    #generate random number
-    rand(10).to_s
-  end
-
-  def rand_char
-    #generating random letter
-    (A_ENCODING + rand(TOTAL_LETTERS_ALPHABET)).chr.to_s
-  end
 
   def generate
   # put your solution here (delete the line below)
   #template "1XC1-A11D-1CDVB-123"
-  rand_int + rand_char + rand_char + rand_int + "-" + #1XC1-
-  rand_char + rand_int + rand_int + rand_char + "-"+ #A11D-
-  rand_int + rand_char + rand_char + rand_char + rand_char + "-"+ #1CDVB-
-  rand_int + rand_int + rand_int #123
+  rand_int = lambda{|x| (1..9).to_a.sample(x).join.to_s} #generate random number
+  rand_char = lambda{|x| ("A".."Z").to_a.sample(x).join}  #generating random letter
+  one = rand_int.call(1) + rand_char.call(2) + rand_int.call(1) #1XC1
+  two = rand_char.call(1) + rand_int.call(2) + rand_char.call(1) #A11D
+  three = rand_int.call(1) + rand_char.call(3) #1CDVB
+  four = rand_int.call(3) #123
+  "#{one}-#{two}-#{three}-#{four}"
   end
