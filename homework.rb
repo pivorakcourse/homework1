e74a3fd60b60c49f7a654d23694d23db4210d66d
Denys Makarenko <makarenko.denys@gmail.com>
2017-06-09 10:26:39 +0300

    Readability improvement

--- homework.rb
+++ homework.rb
@@ -3,7 +3,7 @@ VARIANT = 6
 def generate
   first_part = rand(10..99).to_s
   second_part = rand(1000000..9999999).to_s
-  third_part = [*"A".."Z"].sample(3).join
+  third_part = ("A".."Z").to_a.sample(3).join
 
   "#{first_part}-#{second_part}-#{third_part}"
 end
