a70ddb6d561ac5553d0bdcbd5266b0bde60ab4ee
oleg.matviiv <oleg.matviiv@gmail.com>
2017-06-09 11:57:17 +0300

    define char and num length into constants

--- homework.rb
+++ homework.rb
@@ -1,11 +1,14 @@
-VARIANT     = 15
-NUM_RANGE   = 1..999_999
-CHAR_RANGE  = 'AA'..'ZZ'
+VARIANT       = 15
+NUM_RANGE     = 1..999_999
+NUM_LENGTH    = 6
+LEADING_ZEROS = '0'.freeze
+CHAR_RANGE    = 'AA'..'ZZ'
+CHAR_LENGTH   = 2
 
 # It should fit this pattern "123456-ZXCE-ABCD"
 def generate
-  numbers = -> { rand(NUM_RANGE).to_s.rjust(6, '0') }
-  letters = -> { CHAR_RANGE.to_a.sample(2).join }
+  numbers = -> { rand(NUM_RANGE).to_s.rjust(NUM_LENGTH, LEADING_ZEROS) }
+  letters = -> { CHAR_RANGE.to_a.sample(CHAR_LENGTH).join }
 
-  [numbers.call, letters.call, letters.call].join('-')
+  [numbers, letters, letters].map(&:call).join('-')
 end
