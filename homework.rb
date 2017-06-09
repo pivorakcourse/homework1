1cf99cefb71a3000a04323267f51d9070ad04bda
mrsOwl <whitecapricorni@gmail.com>
2017-06-09 15:51:13 +0300

    next fixes homework1

--- homework.rb
+++ homework.rb
@@ -1,15 +1,14 @@
 VARIANT = 1
 
+
+def random_4_numbers 
+      rand(1000..10000)
+end
+
 def generate 
+  first_random_four_numbers = random_12_numbers
+  second_random_four_numbers = random_12_numbers
+  third_random_four_numbers = random_12_numbers
 
-  def random_12_numbers 
-      return rand(1000..10000)
-  end
-
-
-  first_random_four_numbers = random_12_numbers()
-  second_random_four_numbers = random_12_numbers()
-  third_random_four_numbers = random_12_numbers()
-
-  return "#{first_random_four_numbers}-#{second_random_four_numbers}-#{third_random_four_numbers}"
+  "#{first_random_four_numbers}-#{second_random_four_numbers}-#{third_random_four_numbers}"
 end
