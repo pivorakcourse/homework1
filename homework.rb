517bf7a7942eceb45a6eb908d4b6ee96b924d6f5
mrsOwl <whitecapricorni@gmail.com>
2017-06-09 22:16:27 +0300

    3 fixes

--- homework.rb
+++ homework.rb
@@ -1,14 +1,9 @@
 VARIANT = 1
 
-
 def random_4_numbers 
-      rand(1000..10000)
+  rand(1000..10000)
 end
 
 def generate 
-  first_random_four_numbers = random_4_numbers
-  second_random_four_numbers = random_4_numbers
-  third_random_four_numbers = random_4_numbers
-
-  "#{first_random_four_numbers}-#{second_random_four_numbers}-#{third_random_four_numbers}"
+  "#{random_4_numbers }-#{random_4_numbers }-#{random_4_numbers }"
 end
