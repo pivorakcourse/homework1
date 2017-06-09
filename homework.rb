ef7d9ba50fa73a16b08422bb8e7d7be6e7328ed6
oleg.matviiv <oleg.matviiv@gmail.com>
2017-06-09 10:38:37 +0300

    underline numeric and character range using constants

--- homework.rb
+++ homework.rb
@@ -1,8 +1,11 @@
-VARIANT = 15
+VARIANT     = 15
+NUM_RANGE   = 1..999_999
+CHAR_RANGE  = 'AA'..'ZZ'
 
 # It should fit this pattern "123456-ZXCE-ABCD"
 def generate
-  numbers = -> { rand(100_000..999_999).to_s }
-  letters = -> { ('A'..'Z').to_a.sample(4).join }
+  numbers = -> { rand(NUM_RANGE).to_s.rjust(6, '0') }
+  letters = -> { CHAR_RANGE.to_a.sample(2).join }
+
   [numbers.call, letters.call, letters.call].join('-')
 end
