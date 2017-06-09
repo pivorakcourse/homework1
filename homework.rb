dc3322348763bf4f88d9bf8f2f142cc9a455fd16
Vlad Bilanchuk <burning1848@gmail.com>
2017-06-09 12:18:28 +0300

    change mistake in order

--- homework.rb
+++ homework.rb
@@ -10,14 +10,12 @@ THIRD_PART_LENGTH = 3
 
 # pattern = 'AC-1234567-ABC'
 
-
-
 def generate
 
 
-  first_part = ->()  { (DIGITS_RANGE).to_a.sample(FIRST_PART_LENGTH).join }
-  second_part = ->() { (LETTERS_RANGE).to_a.sample(SECOND_PART_LENGTH).join }
-  third_part = ->() { (DIGITS_RANGE).to_a.sample(THIRD_PART_LENGTH).join }
+  first_part = ->()  { (LETTERS_RANGE).to_a.sample(FIRST_PART_LENGTH).join }
+  second_part = ->() { (DIGITS_RANGE).to_a.sample(SECOND_PART_LENGTH).join }
+  third_part = ->() { (LETTERS_RANGE).to_a.sample(THIRD_PART_LENGTH).join }
 
   result = "#{first_part.call}-#{second_part.call}-#{third_part.call}"
 
