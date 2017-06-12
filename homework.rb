922d0813a70a8f32673982d17e4905aae4a92067
zoroksana <dev.oksana.svyryda@gmail.com>
2017-06-12 10:40:36 +0300

    Create homework.rb
    Correcting mistakes. Is it right?

--- homework.rb
+++ homework.rb
@@ -2,6 +2,6 @@ VARIANT = 1
 # "1234-5678-1234"
 def generate
   # put your solution here (delete the line below)
-  a, b, c = rand(1000...9999), rand(1000...9999), rand(1000...9999)
-  "#{a}" + "-" + "#{b}" + "-" + "#{c}"
+  First_Part, Second_Part, Third_Part = rand(1000...9999)
+  "#{First_Part}" - "#{Second_Part}" - "#{Third_Part}"
 end
