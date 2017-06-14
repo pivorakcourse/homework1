1f97b649cd0ae661603bd75eceaaf0a6a0d655d2
zoroksana <dev.oksana.svyryda@gmail.com>
2017-06-14 07:48:47 +0300

    Change Homework1
    change homework 1

--- homework.rb
+++ homework.rb
@@ -2,6 +2,6 @@ VARIANT = 1
 # "1234-5678-1234"
 def generate
   # put your solution here (delete the line below)
-  First_Part, Second_Part, Third_Part = rand(1000...9999)
+  First_Part, Second_Part, Third_Part =3.times.map {rand(1000...9999).to_s }
   "#{First_Part}" - "#{Second_Part}" - "#{Third_Part}"
 end
